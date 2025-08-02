5923316dc3ab9695306f49065809447aa3a45c57
TheLegendBlack <christ.itoua@efrei.net>
2025-08-02 04:32:45 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API Gestion Locative en ligne !');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
